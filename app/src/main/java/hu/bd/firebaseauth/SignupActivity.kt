12031c4439481a357e8befa2c41b391d272a114f
bdaniel9 <bochisdaniel3@gmail.com>
2025-09-18 21:39:30 +0200

    Filling unnecessary rows

--- app/src/main/java/hu/bd/firebaseauth/SignupActivity.kt
+++ app/src/main/java/hu/bd/firebaseauth/SignupActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import hu.bd.firebaseauth.databinding.ActivitySignupBinding
 
